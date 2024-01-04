1d92da55b6095183d6a2c875157d1636a792fbde
CharlieHavens80654 <112658202+CharlieHavens80654@users.noreply.github.com>
2024-01-04 14:08:08 -0800

    Update whichLanguage.java

--- whichLanguage.java
+++ whichLanguage.java
@@ -19,13 +19,14 @@ public class whichLanguage
         File file = new File("input.txt");
         Scanner sc = new Scanner(file);
         String temp = "";
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        Map<String, Integer> map = new HashMap<String, Integer>();
         while(sc.hasNext()){
             temp = temp + sc.next();
         }
         temp.toLowerCase();
-        for (int i = 0; i < temp.length(); i++) {
-            char c = temp.charAt(i);
+        //creates a character map of all the characters in the input
+        for (int i = temp.length(); i > 0; i--) {
+            String c = temp.substring(i-1, i);
             Integer val = map.get(c);
             if (val != null) {
                 map.put(c, val + 1);
@@ -34,7 +35,21 @@ public class whichLanguage
                map.put(c, 1);
            }
         }
+        int size = (int)map.values().stream().distinct().count();
+        double observed;
+        double output = 0;
+        double expected;
+        for(int i = 0; i < size; i++){
+            //creates the observed percentage of a
+            observed = map.get("a");
+            //creates the expected ammount
+            expected = 0.084966*temp.length();
+            //calcuates chi square thing
+            output = Math.pow((observed-expected), 2);
+            output = output/expected;    
+        }
         System.out.println(map);
+        System.out.println(output);
 
     }
      
